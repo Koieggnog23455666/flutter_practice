fcb93f3b9a845c36fd50fecae9e7b6805145663e
Krishna <krai100200@gmail.com>
2024-01-12 09:37:26 +0530

    Day 8 completed

--- lib/Pages/LoginPage.dart
+++ lib/Pages/LoginPage.dart
@@ -11,82 +11,106 @@ class LoginPage extends StatefulWidget {
 class _LoginPageState extends State<LoginPage> {
   bool ObsecureText=true;
   bool button=false;
-
+final _formKey =GlobalKey<FormState>();
+validateToHome (BuildContext context)async{
+  if(_formKey.currentState!.validate()){
+    setState(() {
+      button=true;
+    });
+    await Future.delayed(Duration(seconds: 1));
+    Navigator.push(context, MaterialPageRoute(builder: (context)=>MyHomePage(title: 'Home')));
+    setState(() {
+      button=false;
+    });
+  }
+}
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      backgroundColor: Colors.white,
-      body: Column(
-        children: [
-        Image.asset('assets/images/undraw login.png',height: 300,),
-          Padding(
-            padding: const EdgeInsets.symmetric(vertical: 16,horizontal: 32),
-            child: Column(
-              children: [
-                TextFormField(
-                decoration: InputDecoration(
-                  labelText: 'USERNAME',
-                  hintText: 'Enter username',
-                  border: OutlineInputBorder(
-                    borderRadius: BorderRadius.circular(12),
-                    borderSide: BorderSide(
-                      width: 6,
-                    )
-                  ),
-
-                ),
-
-                ),
-            SizedBox(
-              height: 20,
-            ),
-                TextFormField(
-
-                  obscureText: ObsecureText,
+    return Form(
+      key: _formKey,
+      child: Scaffold(
+        backgroundColor: Colors.white,
+        body: Column(
+          children: [
+          Image.asset('assets/images/undraw login.png',height: 300,),
+            Padding(
+              padding: const EdgeInsets.symmetric(vertical: 16,horizontal: 32),
+              child: Column(
+                children: [
+                  TextFormField(
                   decoration: InputDecoration(
-                    labelText: 'PASSWORD',
-                    hintText: 'Enter password',
+                    labelText: 'USERNAME',
+                    hintText: 'Enter username',
                     border: OutlineInputBorder(
-                      borderRadius: BorderRadius.circular(12)
-                    ),
-                    suffixIcon:IconButton(color: Colors.redAccent,onPressed: () { setState(() {ObsecureText=!ObsecureText; }); }
-                      ,icon: Icon(ObsecureText?Icons.visibility:Icons.visibility_off) ,
+                      borderRadius: BorderRadius.circular(12),
+                      borderSide: BorderSide(
+                        width: 6,
                       )
+                    ),
+
                   ),
-                ),SizedBox(
-                  height: 20,
-                ),
-                AnimatedContainer(
-                  decoration: BoxDecoration(
-                    borderRadius: BorderRadius.circular(button?30:12)
+                    validator: (value){
+                    if(value!.isEmpty){
+                      return "Username cannot be Empty";
+                    }
+                    return null;
+                    },
+
                   ),
-                  width: button?50:250,
-                  duration: Duration(seconds: 1),
-                  child: ElevatedButton(onPressed: () async {
-                    setState(() {
-                      button=true;
-                    });
-                   await Future.delayed(Duration(seconds: 1));
-                   Navigator.push(context, MaterialPageRoute(builder: (context)=>MyHomePage(title: 'Home')));
-                  }, child:button?FaIcon(Icons.done_outline,color: Colors.white,): Text('Login',style: GoogleFonts.poppins(textStyle: TextStyle(color: Colors.white,fontWeight: FontWeight.bold),)),
-                    style: ElevatedButton.styleFrom(
-                      // primary: Colors.deepPurple,
-                      padding: EdgeInsets.all(0),
-                      fixedSize: Size(250, 50),
-                      shadowColor: Colors.deepPurple,
-                      elevation: 5,
-                      backgroundColor: Colors.deepPurple,
+              SizedBox(
+                height: 20,
+              ),
+                  TextFormField(
+
+                    obscureText: ObsecureText,
+                    decoration: InputDecoration(
+                      labelText: 'PASSWORD',
+                      hintText: 'Enter password',
+                      border: OutlineInputBorder(
+                        borderRadius: BorderRadius.circular(12)
+                      ),
+                      suffixIcon:IconButton(color: Colors.redAccent,onPressed: () { setState(() {ObsecureText=!ObsecureText; }); }
+                        ,icon: Icon(ObsecureText?Icons.visibility:Icons.visibility_off) ,
+                        )
+                    ),
+                    validator: (value){
+                      if(value!.isEmpty){
+                        return "Password cannot be empty";
+                      }
+                      else if(value.length<6){
+                        return "Password should contain atleat 6 characters";
+                      }
+                      return null;
+                    },
+                  ),SizedBox(
+                    height: 20,
+                  ),
+                  AnimatedContainer(
+                    decoration: BoxDecoration(
+                      borderRadius: BorderRadius.circular(button?30:12)
+                    ),
+                    width: button?50:250,
+                    duration: Duration(seconds: 1),
+                    child: ElevatedButton(onPressed: ()=>validateToHome(context), child:button?FaIcon(Icons.done_outline,color: Colors.white,): Text('Login',style: GoogleFonts.poppins(textStyle: TextStyle(color: Colors.white,fontWeight: FontWeight.bold),)),
+                      style: ElevatedButton.styleFrom(
+                        primary: Colors.deepPurple,
+                        padding: EdgeInsets.all(0),
+                        fixedSize: Size(250, 50),
+                        shadowColor: Colors.deepPurple,
+                        elevation: 5,
+                        backgroundColor: Colors.deepPurple,
+
+                      ),
+
 
                     ),
-                    
-
-                  ),
-                )
-              ],
-            ),
-          )
-        ],
-      )
+                  )
+                ],
+              ),
+            )
+          ],
+        )
+      ),
     );
 
   }
